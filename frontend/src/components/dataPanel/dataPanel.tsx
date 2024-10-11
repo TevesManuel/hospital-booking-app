3dbd72293390292c66b8c2a92ae6793a89f6e516
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-10-11 11:56:50 -0300

    Solve coming soon

--- frontend/src/components/dataPanel/dataPanel.tsx
+++ frontend/src/components/dataPanel/dataPanel.tsx
@@ -7,27 +7,25 @@ import ComingSoonPage from "../comingSoonPage/comingSoonPage";
 const DataPanel : React.FC = () => {
     const userValue = useUserValue();
     const location  = useLocation();
-    console.log("asd");
-    console.log(location.pathname.split("/")[-1])
+    console.log();
 
     if(userValue != null)
     {
-        console.log(location.pathname.split("/")[-1])
-        if(location.pathname.split("/")[-1] == 'patients')
+        if(location.pathname.split("/")[2] == 'patients')
         {
             if(userValue.type == 'manager')
             {
                 return ( <ComingSoonPage /> );
             }
         }
-        if(location.pathname.split("/")[-1] == 'medics')
+        if(location.pathname.split("/")[2] == 'medics')
         {
             if(userValue.type == 'manager')
             {
                 return ( <ComingSoonPage /> );
             }
         }
-        if(location.pathname.split("/")[-1] == 'administrators')
+        if(location.pathname.split("/")[2] == 'admins')
         {
             if(userValue.type == 'manager')
             {
